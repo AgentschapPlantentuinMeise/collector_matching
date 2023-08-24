a1d503444904a1449c0e5d1dd8c585846cf977b7
Mathias Dillen <37537540+matdillen@users.noreply.github.com>
2023-08-24 14:28:15 +0200

    add catches for empty strings

--- src/matching.R
+++ src/matching.R
@@ -15,37 +15,46 @@ matchString <- function(name,
     rbind(matches,.)
   
   #exact match of parsed surname and presumed wikidata item surname from label
-  matches = wiki %>%
-    filter(surname == name$surname) %>%
-    select(itemLabel,id) %>%
-    mutate(reason = "surname_match") %>%
-    rbind(matches,.)
+  if (!is.na(name$surname&
+             name$surname!="")) {
+    matches = wiki %>%
+      filter(surname == name$surname) %>%
+      select(itemLabel,id) %>%
+      mutate(reason = "surname_match") %>%
+      rbind(matches,.)
+  }
   
   #exact match of parsed first name and presumed wikidata item first name from label
-  matches = wiki %>%
-    filter(fname == name$fname) %>%
-    select(itemLabel,id) %>%
-    mutate(reason = "firstname_match") %>%
-    rbind(matches,.)
-  
+  if (!is.na(name$fname&
+             name$fname!="")) {
+    matches = wiki %>%
+      filter(fname == name$fname) %>%
+      select(itemLabel,id) %>%
+      mutate(reason = "firstname_match") %>%
+      rbind(matches,.)
+  }
   #fuzzy match of parsed surname into any presumed wikidata item surname from label
   #max distance between both str lengths set by lname_cut
   #default is 2, so surname in wikidata can only be 1 char longer at most
-  matches = wiki %>%
-    filter(agrepl(name$surname,
-                  surname)) %>%
-    filter(nchar(surname)-lname_cut < nchar(name$surname)) %>%
-    select(itemLabel,id) %>%
-    mutate(reason = "surname_fuzzy_match") %>%
-    rbind(matches,.)
-  
+  if (!is.na(name$surname&
+             name$surname!="")) {
+    matches = wiki %>%
+      filter(agrepl(name$surname,
+                    surname)) %>%
+      filter(nchar(surname)-lname_cut < nchar(name$surname)) %>%
+      select(itemLabel,id) %>%
+      mutate(reason = "surname_fuzzy_match") %>%
+      rbind(matches,.)
+  }
   #exact match of the interpreted initials
-  matches = wiki %>%
-    filter(initials == name$initials) %>%
-    select(itemLabel,id) %>%
-    mutate(reason = "initials_match") %>%
-    rbind(matches,.)
-  
+  if (!is.na(name$initials&
+             name$initials!="")) {
+    matches = wiki %>%
+      filter(initials == name$initials) %>%
+      select(itemLabel,id) %>%
+      mutate(reason = "initials_match") %>%
+      rbind(matches,.)
+  }
   if (!is.null(alias)) {
     matches = alias %>%
       filter(itemLabel == name$parsed) %>%
